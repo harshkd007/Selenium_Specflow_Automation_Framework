ddc742929fe0a047dbfcdb5b3f42916ce8260f9b
harshkd007 <kadamharshavardhan042@gmail.com>
2023-05-22 22:30:32 +0530

    Automated Search functionality on YouTube

---
+++ Selenium_Specflow_Framework/Features/YouTube.feature
@@ -0,0 +1,10 @@
+﻿Feature: Youtube
+
+Youtube workflow automation
+
+@tag1
+Scenario: Search Ch. Shivaji Maharaj on youtube
+	Given Open the chrome browser
+	When search 'Ch shivaji Maharaj' on youtube
+	Then I verify the title 'Ch shivaji Maharaj' 
+	When I close browser window
---
+++ Selenium_Specflow_Framework/StepDefinitions/YouTubeStepDefination.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Specflow_Framework.StepDefinitions
+{
+    [Binding]
+    public sealed class YouTubeStepDefination
+    {
+        IWebDriver driver;
+        [Given(@"Open the chrome browser")]
+        public void GivenOpenTheChromeBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        /// <summary>
+        /// Step defination for "Enter text and search on youtube"
+        /// </summary>
+        /// <param name="searchString"></param>
+        [When(@"search '([^']*)' on youtube")]
+        public void WhenSearchOnYoutube(string searchString)
+        {
+            driver.Url = "https://www.youtube.com/";
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(5000);
+            driver.FindElement(By.XPath("//*[@name='search_query']")).SendKeys(searchString);
+            driver.FindElement(By.XPath("//*[@name='search_query']")).SendKeys(Keys.Enter);// to press the enter key
+        }
+
+        /// <summary>
+        /// Step defination for "I verify the title"
+        /// </summary>
+        /// <param name="title"></param>
+        [Then(@"I verify the title '([^']*)'")]
+        public void ThenIVerifyTheTitle(string title)
+        {
+            Thread.Sleep(5000);
+            Assert.IsTrue(driver.Title.ToUpper().Contains(title.ToUpper()));
+        }
+
+        [AfterScenario]
+        /// <summary>
+        /// Step defination for "I close browser window"
+        /// </summary>
+        [When(@"I close browser window")]
+        public void WhenICloseBrowserWindow()
+        {
+            driver.Quit();
+        }
+
+
+
+
+    }
+}
